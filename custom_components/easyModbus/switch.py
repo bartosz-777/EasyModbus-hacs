52c95acd2fc1c0717ba7a18f505a52f72cb2398c
bartosz-777 <81369593+bartosz-777@users.noreply.github.com>
2026-08-02 21:54:48 +0200

    Update switch.py

--- custom_components/easyModbus/switch.py
+++ custom_components/easyModbus/switch.py
@@ -48,10 +48,10 @@ class ModbusSwitch(EbyteM31Entity, SwitchEntity):
 
     def turn_on(self, **kwargs: Any):
         """Turn the switch on."""
-        self.hub.switch_set(self._defn.address, 0 if self._config[CONF_FLIP_OUTPUTS] else 1)
         self._is_on = True
+        self.hub.switch_set(self._defn.address, 0 if self._config[CONF_FLIP_OUTPUTS] else 1)
 
     def turn_off(self, **kwargs: Any):
         """Turn the switch off."""
-        self.hub.switch_set(self._defn.address, 1 if self._config[CONF_FLIP_OUTPUTS] else 0)
         self._is_on = False
+        self.hub.switch_set(self._defn.address, 1 if self._config[CONF_FLIP_OUTPUTS] else 0)
